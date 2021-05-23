313edce0ec574fa642a392e7b23149694789bbae
Kristaps <esmitskristaps@inbox.lv>
2021-05-23 23:00:07 +0300

    pievienoju vairak popup

--- js/admin.js
+++ js/admin.js
@@ -2,3 +2,18 @@ document.getElementById("pievienot").addEventListener("click", () => {
   let popUp = document.getElementById("popUp");
   popUp.style.display = "block";
 });
+
+document.getElementById("labot").addEventListener("click", () => {
+  let popUp1 = document.getElementById("popUp1");
+  popUp1.style.display = "block";
+});
+
+document.getElementById("dzest").addEventListener("click", () => {
+  let popUp2 = document.getElementById("popUp2");
+  popUp2.style.display = "block";
+});
+
+document.getElementById("dzestLidmasinas").addEventListener("click", () => {
+  let popUp5 = document.getElementById("popUp5");
+  popUp5.style.display = "block";
+});
